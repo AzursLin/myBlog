2fbd473e6fd0edcafd9856d6ea9316cb46f417be
kevinlinit <kevinlinit@gmail.com>
2017-08-09 11:22:50 +0800

    1.在MainController处添加代码生成指定格式Excel表，用作达讯Excel导入蓝牙设备，此处备份代码

--- src/main/java/com/lxz/myBlog/controller/MainController.java
+++ src/main/java/com/lxz/myBlog/controller/MainController.java
@@ -4,10 +4,16 @@ package com.lxz.myBlog.controller;
 import com.google.gson.Gson;
 import com.lxz.myBlog.Mapper.userMapper;
 import com.lxz.myBlog.Service.userService;
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.write.Label;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
 import java.util.*;
 
 /**
@@ -20,8 +26,60 @@ public class MainController {
 
     @RequestMapping("/hello")
     public String index(){
-
         Gson gs = new Gson();
-        return gs.toJson(userService.tryLogin("admin","123456"));
+        try {
+            File xlsFile = new File("C://批一蓝牙导入.xls");
+            WritableWorkbook writeBook = Workbook.createWorkbook(xlsFile);
+            WritableSheet firstSheet = writeBook.createSheet("批一蓝牙导入", 1);
+
+            firstSheet.addCell(new Label(0, 0,  "蓝牙名（即镭标）"));
+            firstSheet.addCell(new Label(1, 0,  "蓝牙Mac地址"));
+            firstSheet.addCell(new Label(2, 0,  "蓝牙合法性 1为合法 2为非法"));
+            firstSheet.addCell(new Label(3, 0,  "蓝牙绑定设备类型 "));
+            firstSheet.addCell(new Label(4, 0,  "蓝牙质保时间"));
+
+            String strH,strL;
+            int hNum,lNum;
+            int nameStarNumLen = 400; //长度
+            int nameStarNum = 101; //名字起始
+            int nowNum = 101;  //mac起始地址
+
+
+            for (int i=0;i<nameStarNumLen;i++) {
+                firstSheet.addCell(new Label(0, i+1,  String.format("%06d", nameStarNum+i)));    //蓝牙名
+
+                hNum = (nowNum+i)/256;
+                lNum = (nowNum+i)%256;
+
+                if (hNum < 16) {
+                    strH = "0"+Integer.toHexString(hNum);
+                }
+                else {
+                    strH = Integer.toHexString(hNum);
+                }
+
+                if (lNum < 16) {
+                    strL = "0"+Integer.toHexString(lNum);
+                }
+                else {
+                    strL = Integer.toHexString(lNum);
+                }
+                firstSheet.addCell(new Label(1, i+1,  " 00:DB:00:00:"+strH+":"+strL));
+                firstSheet.addCell(new Label(2, i+1,  "1"));
+                firstSheet.addCell(new Label(3, i+1,  "动感单车"));
+                firstSheet.addCell(new Label(4, i+1,  "3"));
+            }
+            // 4、打开流，开始写文件
+            writeBook.write();
+            writeBook.close();
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            return gs.toJson(e.getMessage());
+        }
+
+
+        return gs.toJson("123");
     }
+
 }
