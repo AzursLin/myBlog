a0d72f556020bd76e1ce6092150a63dae6800fa2
kevinlinit <kevinlinit@gmail.com>
2017-08-15 18:10:25 +0800

    1.修改脚本需求2.把脚本参数集中于前面处理

--- src/main/java/com/lxz/myBlog/controller/MainController.java
+++ src/main/java/com/lxz/myBlog/controller/MainController.java
@@ -28,9 +28,9 @@ public class MainController {
     public String index(){
         Gson gs = new Gson();
         try {
-            File xlsFile = new File("C://批一蓝牙导入.xls");
+            File xlsFile = new File("C://测试蓝牙导入.xls");
             WritableWorkbook writeBook = Workbook.createWorkbook(xlsFile);
-            WritableSheet firstSheet = writeBook.createSheet("批一蓝牙导入", 1);
+            WritableSheet firstSheet = writeBook.createSheet("测试蓝牙导入", 1);
 
             firstSheet.addCell(new Label(0, 0,  "蓝牙名（即镭标）"));
             firstSheet.addCell(new Label(1, 0,  "蓝牙Mac地址"));
@@ -40,33 +40,34 @@ public class MainController {
 
             String strH,strL;
             int hNum,lNum;
-            int nameStarNumLen = 400; //长度
-            int nameStarNum = 101; //名字起始
-            int nowNum = 101;  //mac起始地址
+            int nameStarNumLen = 100; //长度
+            int nameStarNum = 1; //名字起始
+            int nowNum = 1;  //mac起始地址
+            String type = "动感单车";
 
 
             for (int i=0;i<nameStarNumLen;i++) {
-                firstSheet.addCell(new Label(0, i+1,  String.format("%06d", nameStarNum+i)));    //蓝牙名
+                firstSheet.addCell(new Label(0, i+1,  "QiDong"+String.format("%06d", nameStarNum+i)));    //蓝牙名
 
                 hNum = (nowNum+i)/256;
                 lNum = (nowNum+i)%256;
 
                 if (hNum < 16) {
-                    strH = "0"+Integer.toHexString(hNum);
+                    strH = "0"+Integer.toHexString(hNum).toUpperCase();
                 }
                 else {
                     strH = Integer.toHexString(hNum);
                 }
 
                 if (lNum < 16) {
-                    strL = "0"+Integer.toHexString(lNum);
+                    strL = "0"+Integer.toHexString(lNum).toUpperCase();
                 }
                 else {
-                    strL = Integer.toHexString(lNum);
+                    strL = Integer.toHexString(lNum).toUpperCase();
                 }
-                firstSheet.addCell(new Label(1, i+1,  " 00:DB:00:00:"+strH+":"+strL));
+                firstSheet.addCell(new Label(1, i+1,  "00:DB:00:00:"+strH+":"+strL));
                 firstSheet.addCell(new Label(2, i+1,  "1"));
-                firstSheet.addCell(new Label(3, i+1,  "动感单车"));
+                firstSheet.addCell(new Label(3, i+1,  type));
                 firstSheet.addCell(new Label(4, i+1,  "3"));
             }
             // 4、打开流，开始写文件
